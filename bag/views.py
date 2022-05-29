8e1c52559749d76ec66257d5b72dfb439cbcf8d2
Paul Harrington <hello@loosenthedark.tech>
2022-05-29 14:18:02 +0100

    Introduce & test toast messaging functionality & basic user feedback logic

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -8,47 +10,51 @@ def view_bag(request):
 
     return render(request, "bag/bag.html")
 
+
 def add_to_bag(request, item_id):
     """A view to add a quantity of the specified product to the shopping bag"""
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    product = Product.objects.get(pk=item_id)
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    bag = request.session.get("bag", {})
 
     if size:
         if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
+            if size in bag[item_id]["items_by_size"].keys():
+                bag[item_id]["items_by_size"][size] += quantity
             else:
-                bag[item_id]['items_by_size'][size] = quantity
+                bag[item_id]["items_by_size"][size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            bag[item_id] = {"items_by_size": {size: quantity}}
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f"Added {product.name} to your bag!")
 
-    request.session['bag'] = bag
+    request.session["bag"] = bag
 
     return redirect(redirect_url)
 
+
 def adjust_bag(request, item_id):
     """A view to adjust the quantity of the specified product to the specified amount in the shopping bag"""
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get("quantity"))
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    bag = request.session.get("bag", {})
 
     if size:
         if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
+            bag[item_id]["items_by_size"][size] = quantity
         else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
                 bag.pop(item_id)
     else:
         if quantity > 0:
@@ -56,26 +62,27 @@ def adjust_bag(request, item_id):
         else:
             bag.pop(item_id)
 
-    request.session['bag'] = bag
+    request.session["bag"] = bag
 
-    return redirect(reverse('view_bag'))
+    return redirect(reverse("view_bag"))
+
 
 def remove_from_bag(request, item_id):
     """A view to remove the specified product from the shopping bag"""
     try:
         size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        bag = request.session.get('bag', {})
+        if "product_size" in request.POST:
+            size = request.POST["product_size"]
+        bag = request.session.get("bag", {})
 
         if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
                 bag.pop(item_id)
         else:
             bag.pop(item_id)
 
-        request.session['bag'] = bag
+        request.session["bag"] = bag
 
         return HttpResponse(status=200)
 
