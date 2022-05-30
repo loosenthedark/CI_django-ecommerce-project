8a3b104a04ed76046161559df45265f8618c2e56
Paul Harrington <hello@loosenthedark.tech>
2022-05-30 12:06:26 +0100

    Add animated loading overlay to checkout template

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,6 +53,8 @@ form.addEventListener('submit', function (event) {
   event.preventDefault();
   card.update({ 'disabled': true });
   $('#submit-button').attr('disabled', true);
+  $('#payment-form').fadeToggle(100);
+  $('#loading-overlay').fadeToggle(100);
   stripe.confirmCardPayment(clientSecret, {
     payment_method: {
       card: card,
@@ -60,9 +62,14 @@ form.addEventListener('submit', function (event) {
   }).then(function (result) {
     if (result.error) {
       // Show error to your customer (e.g. insufficient funds)
+      const errorDiv = document.getElementById('card-errors');
       const html = `<span class="icon" role="alert"><i class="fas fa-times"></i></span><span>${result.error.message}</span>`;
 
       $(errorDiv).html(html);
+
+      $('#payment-form').fadeToggle(100);
+      $('#loading-overlay').fadeToggle(100);
+
       card.update({ 'disabled': false });
       $('#submit-button').attr('disabled', false);
     } else {
